d6e5fa19e2b995b6f31e6094a5c7c131740ee6be
DESKTOP-E4FR415\Kaupo <ccaups@gmail.com>
2024-05-21 15:10:13 +0300

    21/05/24  15:10

--- StoredProcTARge23/StoredProcTARge23/Data/StoredProcDbContext.cs
+++ StoredProcTARge23/StoredProcTARge23/Data/StoredProcDbContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StoredProcTARge23.Models;
 
 namespace StoredProcTARge23.Data
 {
     public class StoredProcDbContext : DbContext
     {
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options) : base(options) { }
+    public DbSet<Employee> Employees { get; set; }
     }
 }
