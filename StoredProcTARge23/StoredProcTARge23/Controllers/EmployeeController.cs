b1f6a4f36126d7b6c166f7509f100276ef682e38
DESKTOP-E4FR415\Kaupo <ccaups@gmail.com>
2024-05-21 16:01:48 +0300

    21/05/2024 16:01

---
+++ StoredProcTARge23/StoredProcTARge23/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoredProcTARge23.Data;
+using StoredProcTARge23.Models;
+
+namespace StoredProcedureTARge23.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public StoredProcDbContext _context;
+
+        public EmployeeController
+            (
+            StoredProcDbContext context
+            ) 
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _context.Employees
+                    .FromSqlRaw<Employee>("spSearchEmployees")
+                    .ToList();
+
+            return result;
+        }
+    }
+}
